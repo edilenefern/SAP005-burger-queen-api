35e98a87846d64949e76de1cba06af3730934ff7
edilene <edilenefern@gmail.com>
2021-03-27 20:26:23 -0300

    migration

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
+//  require('dotenv').config();
 const express = require('express')
+
+const routes = require('../server/routes')
+
 const cors = require("cors");
-const routes = require('./server/routes/index')
 const app = express()
-const port = 3000
-// const PORT = process.env.PORT || 3000;
+// const port = 3000
+const PORT = process.env.PORT || 3000;
+
 app.use(cors());
 app.use(express.json());
 app.use('/', routes);
-// app.use(routes);
+app.use(routes);
+
 
 app.get('*', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World')
 })
 
-app.listen(port, () => {
+app.listen(PORT, () => {
   console.log(`Example app listening at http://localhost:3000`)
 })
--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,15 @@
-const { Router } = require("express")
-const ExampleRouter = require("./ExampleRouter");
+const { Router } = require("express");
+//  const { routes } = require("./UsersRouter");
+// const ExampleRouter = require("./ExampleRouter");
+const UsersRouter = require("./UsersRouter");
+
+
 
 const router = Router()
 
 // aqui vai todas as rotas
-router.use('/example', ExampleRouter);
+// router.use('/example', ExampleRouter);
+router.use('/user', UsersRouter);
 
 
 module.exports = router
