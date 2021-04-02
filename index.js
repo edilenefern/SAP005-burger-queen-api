39844d776598f27cbc5bd8d36a8da5bda5dd958e
edilene <edilenefern@gmail.com>
2021-04-02 18:05:49 -0300

    initial

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const port = process.env.PORT || 3000;
 const routes = require("./server/routes/index");
 
 const app = express();
 app.use(cors());
-const port = 3000;
+
 app.use(express.json());
 app.use("/", routes);
+
 app.get("*", (req, res) => {
   res.send("Hello World!");
 });
